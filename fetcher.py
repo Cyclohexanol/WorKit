862e702daf1e0ffc0134c39fa0bf1c8b9e6e07af
otto-ben <othman.benchekroun@epfl.ch>
2016-11-20 06:18:22 +0100

    Text interpretation

--- fetcher.py
+++ fetcher.py
@@ -12,7 +12,20 @@ def loop():
     else:
         print("Connection Failed, invalid token?")
 
-def interpret(message):
-    if message.__len__() != 0:
-        print(message[0])
+def interpret(input):
+    if input.__len__() != 0:
+        message = input[0]
+        if 'text' in message:
+            headers = {"Ocp-Apim-Subscription-Key":"3cbc1873c0e847a8b951fab2e549a86a",
+                "Content-Type":"application/json", "Accept":"application/json"}
+            json = jsonify({
+                "documents": [{
+                    "language": "en",
+                    "id": "1",
+                    "text": message['text']
+                }]
+            })
+
+            r = requests.post("https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment", json, headers)
+
     return None
